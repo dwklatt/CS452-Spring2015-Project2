2c5ef695aa6e1a10764e82fe6d8512997423bec7
Derek <dwklatt@gmail.com>
2015-04-19 15:41:52 -0400

    and then there was cube

--- project2.js
+++ project2.js
@@ -1,136 +1,240 @@
 //Derek Klatt and Sean Obyrne
 var gl;
-var points;
-var transX = 0.0;
-var transY = 0.0;
-var canvas;
-var blockSpeed = new Float32Array([
-// thrid bit in each row does nothing other than make array comparison easier blockSpeed[index * 2 / 3 + 1]
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0,
-0,0,0
-]);
 
-var verticesSizes = new Float32Array([
-//why did you add so many?
-//you didnt even add speed and color blocks to acomadtate for the growth
-//i did testing and was never going to grow object before 3 minutes at 4s intervals you had it at 11
-0,-1,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10,
--2,-2,10
-]);
-var program;
-var bufferId;
-var vPosition;
-var cBuffer;
-var vColor;
-var n = 0;
-var vertexColors = [
-vec4( 0.0, 0.0, 0.0, 1.0 ), // black
-vec4( 1.0, 0.0, 0.0, 1.0 ), // red
-vec4( 1.0, 1.0, 0.0, 1.0 ), // yellow
-vec4( 0.0, 0.0, 1.0, 1.0 ), // blue
-vec4( 1.0, 0.0, 0.0, 1.0 ), // red
-vec4( 1.0, 1.0, 0.0, 1.0 ), // yellow
-vec4( 0.0, 0.0, 1.0, 1.0 ), // blue
-vec4( 1.0, 0.0, 0.0, 1.0 ), // red
-vec4( 1.0, 1.0, 0.0, 1.0 ), // yellow
-vec4( 0.0, 0.0, 1.0, 1.0 ), // blue
-vec4( 1.0, 0.0, 0.0, 1.0 ) // red
+var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
+var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+
+var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+var materialShininess = 20.0;
+
+var ctm;
+var ambientColor, diffuseColor, specularColor;
+
+
+var nRows = 50;
+var nColumns = 50;
+
+var pointsArray = [
+// Front face
+	-0.25, -0.25,  0.25,
+	0.25, -0.25,  0.25,
+	0.25,  0.25,  0.25,
+	-0.25,  0.25,  0.25,
+
+	// Back face
+	-0.25, -0.25, -0.25,
+	-0.25,  0.25, -0.25,
+	0.25,  0.25, -0.25,
+	0.25, -0.25, -0.25,
+
+	// Top face
+	-0.25,  0.25, -0.25,
+	-0.25,  0.25,  0.25,
+	0.25,  0.25,  0.25,
+	0.25,  0.25, -0.25,
+
+	// Bottom face
+	-0.25, -0.25, -0.25,
+	0.25, -0.25, -0.25,
+	0.25, -0.25,  0.25,
+	-0.25, -0.25,  0.25,
+
+	// Right face
+	0.25, -0.25, -0.25,
+	0.25,  0.25, -0.25,
+	0.25,  0.25,  0.25,
+	0.25, -0.25,  0.25,
+
+	// Left face
+	-0.25, -0.25, -0.25,
+	-0.25, -0.25,  0.25,
+	-0.25,  0.25,  0.25,
+	-0.25,  0.25, -0.25
 ];
+
+var cubeIndices = [
+	0, 1, 2,      0, 2, 3,    // Front face
+	4, 5, 6,      4, 6, 7,    // Back face
+	8, 9, 10,     8, 10, 11,  // Top face
+	12, 13, 14,   12, 14, 15, // Bottom face
+	16, 17, 18,   16, 18, 19, // Right face
+	20, 21, 22,   20, 22, 23  // Left face
+];
+
+var fColor;
+
+var near = -10;
+var far = 10;
+var radius = 6.0;
+var theta  = 0.0;
+var phi    = 0.25;
+var dr = 5.0 * Math.PI/180.0;
+
+const black = vec4(0.0, 0.0, 0.0, 1.0);
+const red = vec4(1.0, 0.0, 0.0, 1.0);
+
+const at = vec3(0.0, 0.0, 0.0);
+const up = vec3(0.0, 1.0, 0.0);
+
+var left = -2.0;
+var right = 2.0;
+var ytop = 2.0;
+var bottom = -2.0;
+
+var modeViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
+
 var on = [0,0,0,0,0];
 var time = 0;
 var score = 0;
 var timer_interval;
 var render_interval;
 var difficulty = 1;
+
 window.onload = function init()
 {
-	// window size to make the game screen fit
-	var window_w = window.innerWidth;
-	var window_h = window.innerHeight;
-	// timer init
-	var timer = document.getElementById("timer");
-	timer.style.left = window_w/2 + 50 + "px";
-	timer_interval = setInterval("updateTime()", 1000);
-	// moving score under the time
-	var score_board = document.getElementById("score");
-	score_board.style.left = window_w/2 + 50 + "px"
-	canvas = document.getElementById( "gl-canvas" );
-	// canvas.width = window_w/2;
-	//canvas.height = window_h - 40;
-	gl = WebGLUtils.setupWebGL( canvas );
-	if ( !gl ) { alert( "WebGL isn't available" ); }
-	document.onkeydown = keypressed;
-	document.onkeyup = kup;
-	//
-	// Configure WebGL
-	//
-	gl.viewport( 0, 0, canvas.width, canvas.height );
-	gl.clearColor( 0.0, 1.0, 0.0, 1.0 );
-	// Load shaders and initialize attribute buffers
-	program = initShaders( gl, "vertex-shader", "fragment-shader" );
-	gl.useProgram( program );
-	cBuffer = gl.createBuffer();
-	gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-	gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW );
-	vColor = gl.getAttribLocation( program, "vColor" );
-	gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
-	gl.enableVertexAttribArray( vColor );
-	//render every 50 ms
-	render_interval = setInterval(toRender, 10);
-};
+		// window size to make the game screen fit
+		var window_w = window.innerWidth;
+		var window_h = window.innerHeight;
+		// timer init
+		var timer = document.getElementById("timer");
+		timer.style.left = window_w/2 + 50 + "px";
+		timer_interval = setInterval("updateTime()", 1000);
+		// moving score under the time
+		var score_board = document.getElementById("score");
+		score_board.style.left = window_w/2 + 50 + "px"
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    // enable depth testing and polygon offset
+    // so lines will be in front of filled triangles
+    
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.enable(gl.POLYGON_OFFSET_FILL);
+    gl.polygonOffset(1.0, 2.0);
+
+// vertex array of nRows*nColumns quadrilaterals 
+// (two triangles/quad) from data
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    ambientProduct = mult(lightAmbient, materialAmbient);
+    diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    specularProduct = mult(lightSpecular, materialSpecular);
+
+    var vBufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+    
+    // Color data
+    var colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    var faceColors = [
+        [1.0, 0.0, 0.0, 1.0], // Front face
+        [0.0, 1.0, 0.0, 1.0], // Back face
+        [0.0, 0.0, 1.0, 1.0], // Top face
+        [1.0, 1.0, 0.0, 1.0], // Bottom face
+        [1.0, 0.0, 1.0, 1.0], // Right face
+        [0.0, 1.0, 1.0, 1.0]  // Left face
+    ];
+    var vertexColors = [];
+    for (var i in faceColors) {
+        var color = faceColors[i];
+        for (var j=0; j < 4; j++) {
+            vertexColors = vertexColors.concat(color);
+        }
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
+    
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+    
+    var cubeIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeIndexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeIndices), gl.STATIC_DRAW);
+    
+    fColor = gl.getUniformLocation(program, "fColor");
+ 
+    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+    
+    gl.uniform4fv( gl.getUniformLocation(program, 
+       "ambientProduct"),flatten(ambientProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program, 
+       "diffuseProduct"),flatten(diffuseProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program, 
+       "specularProduct"),flatten(specularProduct) );	
+    gl.uniform4fv( gl.getUniformLocation(program, 
+       "lightPosition"),flatten(lightPosition) );
+    gl.uniform1f( gl.getUniformLocation(program, 
+       "shininess"),materialShininess );
+
+    render_interval = setInterval(render, 10);
+}
+
+function render()
+{
+			//left
+		if(on[0] == 1){
+		}
+		//up
+		if(on[1] == 1){
+			//put jump here?
+		}
+		//right
+		if(on[2] == 1){
+		}
+		//down
+		if(on[3] == 1){
+		}
+		//space
+		if(on[4] == 1){
+			//put jump here
+		}
+		
+		updateObjects();
+		if (hasLost()) { newGame(); }
+		
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    
+    var eye = vec3( radius*Math.sin(theta)*Math.cos(phi), 
+                    radius*Math.cos(theta)*Math.sin(phi),
+                    radius*Math.cos(theta));
+    
+    modelViewMatrix = lookAt( eye, at, up );
+    projectionMatrix = ortho( left, right, bottom, ytop, near, far );
+    
+    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+
+		gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+    requestAnimFrame(render);
+}
+
 //Starts a new game
 function newGame() {
 	time = 0;
 	var timer = document.getElementById("timer");
 	timer.innerHTML = "<p>Time: " + time + "</p>";
-	verticesSizes[0] = 0;
-	verticesSizes[1] = 0;
-	verticesSizes[2] = 10;
-	for(var i = 3; i < verticesSizes.length; i+=3){
-		verticesSizes[i] = -2;
-		verticesSizes[i+1] = -2;
-		verticesSizes[i+2] = 10;
-	}
-	transX = 0.0;
-	transY = 0.0;
 	on[0] = 0;
 	on[1] = 0;
 	on[2] = 0;
@@ -144,32 +248,18 @@ function restart() {
 	var button = document.getElementById("newGame");
 	button.style.visibility = "hidden";
 	timer_interval = setInterval("updateTime()", 1000);
-	render_interval = setInterval(toRender, 10);
+	render_interval = setInterval(render, 10);
 }
 //Collision detection
 function hasLost() {
-	for(var i = 3; i < verticesSizes.length; i+=3){
-		var xdiff = verticesSizes[0] - verticesSizes[i];
-		var ydiff = verticesSizes[1] - verticesSizes[i+1];
-		if ((-0.0355 <= xdiff && xdiff <= 0) || (0 <= xdiff && xdiff <= 0.0355)) {
-			if ((-0.0355 <= ydiff && ydiff <= 0) || (0 <= ydiff && ydiff <= 0.0355)) {
-				clearInterval(render_interval);
-				clearInterval(timer_interval);
-				return true;
-			}
-		}
-	}
 	return false;
 }
 function updateTime() {
 	time++;
 	var timer = document.getElementById("timer");
 	timer.innerHTML = "<p>Time: " + time + "</p>";
-	// verticesSizes[2] += 0.1; // way to easy
 	if(time%4 == 3){
-		if(difficulty * 3 < verticesSizes.length){ difficulty++; }
-		else { verticesSizes[2] += 0.3; }
-		//console.log(difficulty*3, verticesSizes.length)
+		difficulty++;
 	}
 	if(time > score){
 		score = time;
@@ -177,71 +267,7 @@ function updateTime() {
 		highscore.innerHTML = "<p>High Score: " + score + "</p>"; 
 	}
 }
-function toRender(){
-	//left
-	if(on[0] == 1){
-		verticesSizes[0] -= 0.01;
-	}
-	//up
-	if(on[1] == 1){
-		//verticesSizes[1] += 0.01;
-		//put jump here?
-	}
-	//right
-	if(on[2] == 1){
-		verticesSizes[0] += 0.01;
-	}
-	//down
-	if(on[3] == 1){
-		//verticesSizes[1] -= 0.01;
-	}
-	//space
-	if(on[4] == 1){
-		//put jump here
-	}
-	//detect player hitting wall
-	if(verticesSizes[0] > 1.0-verticesSizes[2]/500)
-		verticesSizes[0] = 1.0-verticesSizes[2]/500;
-	if(verticesSizes[0] < -1.0+verticesSizes[2]/500)
-		verticesSizes[0] = -1.0+verticesSizes[2]/500;
-	if(verticesSizes[1] > 1.0-verticesSizes[2]/500)
-		verticesSizes[1] = 1.0-verticesSizes[2]/500;
-	if(verticesSizes[1] < -1.0+verticesSizes[2]/500)
-		verticesSizes[1] = -1.0+verticesSizes[2]/500;
-	//other wall collision
-	updateObjects();
-	if (hasLost()) { newGame(); }
-	//developer hax
-	//verticesSizes[0] = transX;
-	//verticesSizes[1] = transY;
-	n = 11;
-	// Create a buffer object
-	var vertexSizeBuffer = gl.createBuffer();
-	if (!vertexSizeBuffer) {
-		console.log('Failed to create the buffer object');
-	}
-	// Bind the buffer object to target
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertexSizeBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, verticesSizes, gl.STATIC_DRAW);
-	var FSIZE = verticesSizes.BYTES_PER_ELEMENT;
-	//Get the storage location of a_Position, assign and enable buffer
-	var vPosition = gl.getAttribLocation(program, 'vPosition');
-	if (vPosition < 0) {
-		console.log('Failed to get the storage location of vPosition');
-	}
-	gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, FSIZE * 3, 0);
-	gl.enableVertexAttribArray(vPosition); // Enable the assignment of the buffer object
-	// Get the storage location of a_PointSize
-	var a_PointSize = gl.getAttribLocation(program, 'a_PointSize');
-	if(a_PointSize < 0) {
-		console.log('Failed to get the storage location of a_PointSize');
-	}
-	gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, FSIZE * 3, FSIZE * 2);
-	gl.enableVertexAttribArray(a_PointSize); // Enable buffer allocation
-	// Unbind the buffer object
-	gl.bindBuffer(gl.ARRAY_BUFFER, null);
-	render();
-}
+
 function keypressed(event) {
 	//left
 	if(event.keyCode == '37')
@@ -271,30 +297,7 @@ function kup(event) {
 	else if(event.keyCode == '32')
 		on[4] = 0;
 }
-function render() {
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	// Draw three points
-	gl.drawArrays(gl.POINTS, 0, n);
-}
+
 function updateObjects(){
-	for(i = 3; i < difficulty * 3 + 1; i +=3){
-	// if collision with a wall
-		if(verticesSizes[i] > 1.0-verticesSizes[i+2]/500 || verticesSizes[i] < -1.0+verticesSizes[i+2]/500 || verticesSizes[i+1] > 1.0-verticesSizes[i+2]/500 || verticesSizes[i+1] < -1.0+verticesSizes[i+2]/500)
-			makeNewObjects(i);
-	//move object
-		verticesSizes[i] += blockSpeed[i];
-		verticesSizes[i+1] += blockSpeed[i+1];
-	}
-}
-function makeNewObjects(index){
-	// 0-1 + (0 or -1)
-	var wall = 0; //= Math.floor(Math.random() * 4); // 0-3
-	var randomPlace = Math.random() * 2 - 1;
-	//var direction = 0.01 * Math.pow(-1, Math.round(Math.random() * 2)) * Math.round(randomPlace);
-	if(wall == 0){
-		verticesSizes[index] = randomPlace;
-		verticesSizes[index + 1] = 1.0-verticesSizes[i+2]/500; // top wall
-		blockSpeed[index] = 0;
-		blockSpeed[index + 1] = -0.01; // move down
-	}
+
 }
